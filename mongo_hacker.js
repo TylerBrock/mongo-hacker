87844d6a0406824dc55b0756feb44c1019efeea0
Nigel McNie <nigel@mcnie.name>
2012-11-16 17:20:06 +1300

    Add "table output" support to cursor objects.
    As initially developed at https://github.com/nigelmcnie/mongo-tabular
    
    This allows you to call .t() on cursor objects to get a RDBMS-style
    "table" output of the data in the cursor.

--- mongo_hacker.js
+++ mongo_hacker.js
@@ -9,6 +9,17 @@
  *
  */
 
+var _tabular_defaults = {
+    // How many rows to show by default
+    limit: 20,
+
+    // Columns longer than this will be truncated
+    maxlen: 50,
+
+    // Undefined field values will be output using this string
+    undef: '',
+};
+
 __ansi = {
     csi: String.fromCharCode(0x1B) + '[',
     reset: '0',
@@ -385,3 +396,100 @@ DBQuery.prototype.shellPrint = function(){
     }
 
 }
+
+_tabular_encode = function(obj, opts, tojson) {
+    if ( obj === undefined ) {
+        return opts.undef;
+    }
+    if ( obj instanceof ObjectId ) {
+        return obj.valueOf();
+    }
+    ret = tojson(obj, false, true);
+
+    if ( ret.length > opts.maxlen ) {
+        ret = ret.slice(0, opts.maxlen - 3) + '...';
+        ret = ret.replace("\t", "", 'g')
+    }
+
+    return ret;
+};
+DBQuery.prototype.tabular = function(opts) {
+    if ( typeof(opts) == 'object' ) {
+        opts.limit  = (typeof(opts.limit)  != 'undefined') ? opts.limit  : _tabular_defaults.limit;
+        opts.maxlen = (typeof(opts.maxlen) != 'undefined') ? opts.maxlen : _tabular_defaults.maxlen;
+        opts.undef  = (typeof(opts.undef)  != 'undefined') ? opts.undef  : _tabular_defaults.undef;
+    }
+    else if ( typeof(opts) == 'number' ) {
+        opts.limit  = opts;
+        opts.maxlen = _tabular_defaults.maxlen;
+        opts.undef  = _tabular_defaults.undef;
+    }
+    else {
+        opts = _tabular_defaults;
+    }
+
+    var field_length = { '_id': 24 };
+
+    var docs = this.limit(opts.limit).toArray();
+
+    docs.forEach(function(doc) {
+        var field;
+        for ( field in doc ) {
+            var value = _tabular_encode(doc[field], opts, tojson);
+            if ( !field_length[field] ) {
+                field_length[field] = field.length;
+            }
+            if ( field_length[field] < value.length ) {
+                field_length[field] = value.length;
+            }
+        }
+    });
+
+    var field;
+    var fields = [];
+    var output = '';
+
+    for ( field in field_length ) {
+        fields.push(field);
+    }
+
+    // Divider
+    var divider = '+';
+    fields.forEach(function(field) {
+        var width = field_length[field];
+        divider += Array(width+3).join('-');
+        divider += '+';
+    });
+
+    var header = '| ';
+    // Title row
+    fields.forEach(function(field) {
+        var width = field_length[field];
+        var lpad = Math.floor((width - field.length)/2);
+        var rpad = width - lpad - field.length;
+        header += Array(lpad+1).join(' ') + field + Array(rpad+1).join(' ');
+        header += ' | ';
+    });
+
+    output += divider + "\n";
+    output += header + "\n";
+    output += divider + "\n";
+
+    // Rows
+    docs.forEach(function(doc) {
+        var row = '| ';
+        fields.forEach(function(field) {
+            var value    = _tabular_encode(doc[field], opts, tojson);
+            var rawvalue = _tabular_encode(doc[field], opts, tojson);
+            var width = field_length[field];
+            var rpad = Math.floor(width - rawvalue.length);
+            row += value + Array(rpad+1).join(' ');
+            row += ' | ';
+        });
+        output += row + "\n";
+    });
+
+    output += divider + "\n";
+    return output;
+};
+DBQuery.prototype.t = DBQuery.prototype.tabular;
