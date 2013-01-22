35cd3717c3910c23af2ba676a13009b2bd6b8356
Hannes Magnusson <bjori@10gen.com>
2013-01-22 17:56:10 -0500

    Add a config.js template listing all currently-supported-variables

---
+++ config.js
@@ -0,0 +1,17 @@
+VERBOSE_SHELL = true;
+
+/* Always show information about index use */
+INDEX_PARANOIA = true;
+/* We all have our own indention styles.. */
+INDENT = "  ";
+
+/* Syntax highlight json objects */
+/* Valid color names are: red, green, yellow, blue, magenta, cyan */
+COLOR_NUMBERS = "blue";
+COLOR_NULL    = "red";
+COLOR_UNDEFINED = "magenta";
+COLOR_OBJECTID  = "green";
+COLOR_STRING    = "green";
+COLOR_BOOLEAN   = "blue";
+COLOR_FUNCTION  = "magenta";
+
