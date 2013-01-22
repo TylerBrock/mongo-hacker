20310e15206f5415a9ae686f715231940852ae8d
Mike O'Brien <mikeo@10gen.com>
2013-01-22 17:05:34 -0500

    don't error out if shell is started with --norc

--- mongo_hacker.js
+++ mongo_hacker.js
@@ -31,10 +31,12 @@ if (_isWindows()) {
   print("\nSorry! MongoDB Shell Enhancements for Hackers isn't compatible with Windows.\n");
 }
 
-var ver = db.version().split(".");
-if ( ver[0] <= parseInt("2") && ver[1] < parseInt("2") ) {
-  print(colorize("\nSorry! Mongo version 2.2.x and above is required! Please upgrade.\n", "red", true));
-} 
+if(db){ // skip check optimistically, if the shell was started with --nodb
+    var ver = db.version().split(".");
+    if ( ver[0] <= parseInt("2") && ver[1] < parseInt("2") ) {
+      print(colorize("\nSorry! Mongo version 2.2.x and above is required! Please upgrade.\n", "red", true));
+    } 
+}
 
 setVerboseShell(true);
 setIndexParanoia(true);
