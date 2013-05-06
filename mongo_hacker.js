f3f5dacd73c927756c963d8f795b0b762df0dfd0
Tyler Brock <tyler.brock@gmail.com>
2013-05-06 11:06:52 -0400

    fixes #44 and makes sh.status() really nice

--- mongo_hacker.js
+++ mongo_hacker.js
@@ -836,3 +836,80 @@ shellHelper.show = function (what) {
 
 }
 
+printShardingStatus = function( configDB , verbose ){
+    if (configDB === undefined)
+        configDB = db.getSisterDB('config')
+
+    var version = configDB.getCollection( "version" ).findOne();
+    if ( version == null ){
+        print( "printShardingStatus: this db does not have sharding enabled. be sure you are connecting to a mongos from the shell and not to a mongod." );
+        return;
+    }
+
+    var raw = "";
+    var output = function(s){
+        raw += s + "\n";
+    }
+    output( "--- Sharding Status --- " );
+    output( "  sharding version: " + tojson( configDB.getCollection( "version" ).findOne(), "  " ) );
+
+    output( "  shards:" );
+    configDB.shards.find().sort( { _id : 1 } ).forEach(
+        function(z){
+            output( "    " + tojsononeline( z ) );
+        }
+    );
+
+    output( "  databases:" );
+    configDB.databases.find().sort( { name : 1 } ).forEach(
+        function(db){
+            output( "    " + tojsononeline(db,"",true) );
+
+            if (db.partitioned){
+                configDB.collections.find( { _id : new RegExp( "^" +
+                    RegExp.escape(db._id) + "\\." ) } ).
+                    sort( { _id : 1 } ).forEach( function( coll ){
+                        if ( coll.dropped == false ){
+                            output( "    " + coll._id );
+                            output( "      shard key: " + tojson(coll.key, 0, true) );
+                            output( "      chunks:" );
+
+                            res = configDB.chunks.group( { cond : { ns : coll._id } , key : { shard : 1 },
+                                reduce : function( doc , out ){ out.nChunks++; } , initial : { nChunks : 0 } } );
+                            var totalChunks = 0;
+                            res.forEach( function(z){
+                                totalChunks += z.nChunks;
+                                output( "        " + z.shard + "\t" + z.nChunks );
+                            } )
+
+                            if ( totalChunks < 20 || verbose ){
+                                configDB.chunks.find( { "ns" : coll._id } ).sort( { min : 1 } ).forEach(
+                                    function(chunk){
+                                        output( "        " +
+                                            tojson( chunk.min, 0, true) + " -> " +
+                                            tojson( chunk.max, 0, true ) +
+                                            " on: " + colorize(chunk.shard, "cyan") + " " +
+                                            ( chunk.jumbo ? "jumbo " : "" )
+                                        );
+                                    }
+                                );
+                            }
+                            else {
+                                output( "\t\t\ttoo many chunks to print, use verbose if you want to force print" );
+                            }
+
+                            configDB.tags.find( { ns : coll._id } ).sort( { min : 1 } ).forEach(
+                                function( tag ) {
+                                    output( "\t\t\t tag: " + tag.tag + "  " + tojson( tag.min ) + " -->> " + tojson( tag.max ) );
+                                }
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    );
+
+    print( raw );
+}
+
