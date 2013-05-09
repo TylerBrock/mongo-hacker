3041d889a509f1a9ff37ccf3a78f0d1d229a764b
Tyler Brock <tyler.brock@gmail.com>
2013-05-08 23:21:35 -0400

    new aggreagation framework api helper

--- mongo_hacker.js
+++ mongo_hacker.js
@@ -916,3 +916,111 @@ printShardingStatus = function( configDB , verbose ){
     print( raw );
 }
 
+function hasDollar(fields){
+    for (k in fields){
+        if(k.indexOf('$') !== -1){
+            return true;
+        };
+    };
+    return false;
+}
+
+// Aggregate extension to support alternate API
+DBCollection.prototype.aggregate = function( fields ){
+    if(arguments.length > 1 || hasDollar(fields) || hasDollar(fields[0])){
+        var arr = fields;
+
+        if (!fields.length) {
+            arr = [];
+            for (var i=0; i<arguments.length; i++) {
+                arr.push(arguments[i]);
+            }
+        }
+
+        var res = this.runCommand("aggregate", {pipeline: arr});
+        if (!res.ok) {
+            printStackTrace();
+            throw "aggregate failed: " + tojson(res);
+        }
+        return res;
+    } else {
+       return new AggHelper( this ).match( fields || {} );
+    }
+};
+
+// Aggregation Framework Helper
+AggHelper = function( collection, fields ){
+    this.collection = collection;
+    this.pipeline = [];
+};
+
+AggHelper.prototype.execute = function(){
+    var res = this.collection.runCommand("aggregate", {pipeline: this.pipeline});
+    if (!res.ok) {
+        printStackTrace();
+        throw "aggregate failed: " + tojson(res);
+    }
+    return res;
+};
+
+AggHelper.prototype.shellPrint = function(){
+    this.execute().result.forEach(function(result){
+        printjson(result);
+    });
+};
+
+AggHelper.prototype.project = function( fields ){
+    if(!fields){
+        throw "project needs fields";
+    }
+    this.pipeline.push({"$project" : fields});
+    return this;
+};
+
+AggHelper.prototype.match = function( criteria ){
+    if(!criteria){
+        throw "match needs a query object";
+    }
+    this.pipeline.push({"$match" : criteria});
+    return this;
+};
+
+AggHelper.prototype.limit = function( limit ){
+    if(!limit){
+        throw "limit needs an integer indicating the max number of documents to limit";
+    }
+    this.pipeline.push({"$limit" : limit});
+    return this;
+};
+
+AggHelper.prototype.skip = function( skip ){
+    if(!skip){
+        throw "skip needs an integer indicating the number of documents to skip";
+    }
+    this.pipeline.push({"$skip" : skip});
+    return this;
+};
+
+AggHelper.prototype.unwind = function( field ){
+    if(!field){
+        throw "unwind needs a string indicating the key of an array field to unwind";
+    }
+    this.pipeline.push({"$unwind" : "$" + field});
+    return this;
+};
+
+AggHelper.prototype.group = function( group, group_expression ){
+    if(!group_expression) group_expression = {};
+    if(!group){
+        throw "group needs an grouping key";
+    }
+    group_expression._id = "$" + group;
+    this.pipeline.push({"$group" : group_expression});
+    return this;
+};
+
+AggHelper.prototype.sort = function( sort ){
+    this.pipeline.push({"$sort" : sort});
+    return this;
+};
+
