448c84ab76984982aa8451634e47812b834e5988
Gianfranco <gianpa@gmail.com>
2015-03-23 00:15:05 +0000

    include balancer state in sh.status() as per 3.0 shell

--- hacks/sh_status.js
+++ hacks/sh_status.js
@@ -1,3 +1,23 @@
+sh.getRecentMigrations = function () {
+    var configDB = db.getSiblingDB("config");
+    var yesterday = new Date( new Date() - 24 * 60 * 60 * 1000 );
+    var result = [];
+    result = result.concat(configDB.changelog.aggregate( [
+        { $match : { time : { $gt : yesterday }, what : "moveChunk.from", "details.errmsg" : {
+            "$exists" : false } } },
+        { $group : { _id: { msg: "$details.errmsg" }, count : { "$sum":1 } } },
+        { $project : { _id : { $ifNull: [ "$_id.msg", "Success" ] }, count : "$count" } }
+    ] ).result);
+    result = result.concat(configDB.changelog.aggregate( [
+        { $match : { time : { $gt : yesterday }, what : "moveChunk.from", "details.errmsg" : {
+            "$exists" : true } } },
+        { $group : { _id: { msg: "$details.errmsg", from : "$details.from", to: "$details.to" },
+            count : { "$sum":1 } } },
+        { $project : { _id : "$_id.msg" , from : "$_id.from", to : "$_id.to" , count : "$count" } }
+    ] ).result);
+    return result;
+};
+
 printShardingStatus = function( configDB , verbose ){
     if (configDB === undefined)
         configDB = db.getSisterDB('config')
@@ -22,6 +42,81 @@ printShardingStatus = function( configDB , verbose ){
         }
     );
 
+    // All of the balancer information functions below depend on a connection to a liveDB
+    // This isn't normally a problem, but can cause issues in testing and running with --nodb
+    if ( typeof db !== "undefined" ) {
+        output( "  balancer:" );
+
+        //Is the balancer currently enabled
+        output( "\tCurrently enabled:  " + ( sh.getBalancerState() ? colorize("yes", {color: "cyan"}) : colorize("no", {color: "red"}) ) );
+
+        //Is the balancer currently active
+        output( "\tCurrently running:  " + colorize(( sh.isBalancerRunning() ? "yes" : "no" ), {color: "gray"}) );
+
+        //Output details of the current balancer round
+        var balLock = sh.getBalancerLockDetails();
+        if ( balLock ) {
+            output( "\t\tBalancer lock taken at " + colorize(balLock.when, {color: "gray"}) + " by " + colorize(balLock.who, {color: "cyan"}) );
+        }
+
+        //Output the balancer window
+        var balSettings = sh.getBalancerWindow();
+        if ( balSettings ) {
+            output( "\t\tBalancer active window is set between " +
+                colorize(balSettings.start, {color: "gray"}) + " and " + colorize(balSettings.stop, {color: "gray"}) + " server local time");
+        }
+
+        //Output the list of active migrations
+        var activeMigrations = sh.getActiveMigrations();
+        if (activeMigrations.length > 0 ){
+            output("\tCollections with active migrations: ");
+            activeMigrations.forEach( function(migration){
+                output("\t\t"+colorize(migration._id, {color: "cyan"})+ " started at " + colorize(migration.when, {color: "gray"}) );
+            });
+        }
+
+        // Actionlog and version checking only works on 2.7 and greater
+        var versionHasActionlog = false;
+        var metaDataVersion = configDB.getCollection("version").findOne().currentVersion;
+        if ( metaDataVersion > 5 ) {
+            versionHasActionlog = true;
+        }
+        if ( metaDataVersion == 5 ) {
+            var verArray = db.serverBuildInfo().versionArray;
+            if (verArray[0] == 2 && verArray[1] > 6){
+                versionHasActionlog = true;
+            }
+        }
+
+        if ( versionHasActionlog ) {
+            //Review config.actionlog for errors
+            var actionReport = sh.getRecentFailedRounds();
+            //Always print the number of failed rounds
+            output( "\tFailed balancer rounds in last 5 attempts:  " + colorize(actionReport.count, {color: "red"}) );
+
+            //Only print the errors if there are any
+            if ( actionReport.count > 0 ){
+                output( "\tLast reported error:  " + actionReport.lastErr );
+                output( "\tTime of Reported error:  " + actionReport.lastTime );
+            }
+
+            output("\tMigration Results for the last 24 hours: ");
+            var migrations = sh.getRecentMigrations();
+            if(migrations.length > 0) {
+                migrations.forEach( function(x) {
+                    if (x._id === "Success"){
+                        output( "\t\t" + colorize(x.count, {color: "gray"}) + " : " + colorize(x._id, {color: "cyan"}));
+                    } else {
+                        output( "\t\t" + colorize(x.count, {color: "gray"}) + " : Failed with error '" + colorize(x._id, {color: "red"}) +
+                        "', from " + x.from + " to " + x.to );
+                    }
+                });
+            } else {
+                    output( "\t\tNo recent migrations");
+            }
+        }
+    }
+
     output( "  databases:" );
     configDB.databases.find().sort( { name : 1 } ).forEach(
         function(db){
@@ -38,13 +133,15 @@ printShardingStatus = function( configDB , verbose ){
 
                             res = configDB.chunks.aggregate(
                                 { "$match": { ns: coll._id } },
-                                { "$group": { _id: "$shard", nChunks: { "$sum": 1 } } }
+                                { "$group": { _id: "$shard", nChunks: { "$sum": 1 } } },
+                                { "$project" : { _id : 0 , shard : "$_id" , nChunks : "$nChunks" } },
+                                { "$sort" : { shard : 1 } }
                             ).result
 
                             var totalChunks = 0;
                             res.forEach( function(z){
                                 totalChunks += z.nChunks;
-                                output( "        " + z._id + ": " + z.nChunks );
+                                output( "        " + z.shard + ": " + z.nChunks );
                             } )
 
                             if ( totalChunks < 20 || verbose ){
@@ -53,7 +150,7 @@ printShardingStatus = function( configDB , verbose ){
                                         output( "        " +
                                             tojson( chunk.min, 0, true) + " -> " +
                                             tojson( chunk.max, 0, true ) +
-                                            " on: " + colorize(chunk.shard, {color: 'cyan'}) + " " +
+                                            " on: " + colorize(chunk.shard, {color: 'cyan'}) + " " + tojson( chunk.lastmod ) + " " +
                                             ( chunk.jumbo ? "jumbo " : "" )
                                         );
                                     }
