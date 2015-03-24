62179bf3cafcf763ba56686e4b2237416fe2e00e
Gianfranco <gianpa@gmail.com>
2015-03-24 00:57:04 +0000

    jshint sh_status.js stricter

--- hacks/sh_status.js
+++ hacks/sh_status.js
@@ -18,7 +18,7 @@ sh.getRecentMigrations = function () {
     return result;
 };
 
-printShardingStatus = function( configDB , verbose ){
+printShardingStatus = function( configDB , verbose ) {
     if (configDB === undefined)
         configDB = db.getSisterDB("config");
 
@@ -29,7 +29,7 @@ printShardingStatus = function( configDB , verbose ){
     }
 
     var raw = "";
-    var output = function(s){
+    var output = function(s) {
         raw += s + "\n";
     };
     output( "--- Sharding Status --- " );
@@ -37,7 +37,7 @@ printShardingStatus = function( configDB , verbose ){
 
     output( "  shards:" );
     configDB.shards.find().sort( { _id : 1 } ).forEach(
-        function(z){
+        function(z) {
             output( "    " + tojsononeline( z ) );
         }
     );
@@ -118,58 +118,55 @@ printShardingStatus = function( configDB , verbose ){
     }
 
     output( "  databases:" );
-    configDB.databases.find().sort( { name : 1 } ).forEach(
-        function(db){
-            output( "    " + tojsononeline(db,"",true) );
+    configDB.databases.find().sort( { name : 1 } ).forEach( function(db) {
+        output( "    " + tojsononeline(db,"",true) );
 
-            if (db.partitioned){
-                configDB.collections.find( { _id : new RegExp( "^" +
-                    RegExp.escape(db._id) + "\\." ) } ).
-                    sort( { _id : 1 } ).forEach( function( coll ){
-                        if ( coll.dropped === false ){
-                            output( "    " + coll._id );
-                            output( "      shard key: " + tojson(coll.key, 0, true) );
-                            output( "      chunks:" );
+        if (db.partitioned) {
+            configDB.collections.find( { _id : new RegExp( "^" +
+                RegExp.escape(db._id) + "\\." ) } ).sort( { _id : 1 } ).forEach( function( coll ) {
+                    if ( coll.dropped === false ) {
+                        output( "    " + coll._id );
+                        output( "      shard key: " + tojson(coll.key, 0, true) );
+                        output( "      chunks:" );
 
-                            var res = configDB.chunks.aggregate(
-                                { "$match": { ns: coll._id } },
-                                { "$group": { _id: "$shard", nChunks: { "$sum": 1 } } },
-                                { "$project" : { _id : 0 , shard : "$_id" , nChunks : "$nChunks" } },
-                                { "$sort" : { shard : 1 } }
-                            ).result;
+                        var res = configDB.chunks.aggregate(
+                            { "$match": { ns: coll._id } },
+                            { "$group": { _id: "$shard", nChunks: { "$sum": 1 } } },
+                            { "$project" : { _id : 0 , shard : "$_id" , nChunks : "$nChunks" } },
+                            { "$sort" : { shard : 1 } }
+                        ).result;
 
-                            var totalChunks = 0;
-                            res.forEach( function(z){
-                                totalChunks += z.nChunks;
-                                output( "        " + z.shard + ": " + z.nChunks );
-                            } );
+                        var totalChunks = 0;
+                        res.forEach( function(z) {
+                            totalChunks += z.nChunks;
+                            output( "        " + z.shard + ": " + z.nChunks );
+                        } );
 
-                            if ( totalChunks < 20 || verbose ){
-                                configDB.chunks.find( { "ns" : coll._id } ).sort( { min : 1 } ).forEach(
-                                    function(chunk){
-                                        output( "        " +
-                                            tojson( chunk.min, 0, true) + " -> " +
-                                            tojson( chunk.max, 0, true ) +
-                                            " on: " + colorize(chunk.shard, {color: "cyan"}) + " " + tojson( chunk.lastmod ) + " " +
-                                            ( chunk.jumbo ? "jumbo " : "" )
-                                        );
-                                    }
-                                );
-                            }
-                            else {
-                                output( "\t\t\ttoo many chunks to print, use verbose if you want to force print" );
-                            }
-
-                            configDB.tags.find( { ns : coll._id } ).sort( { min : 1 } ).forEach(
-                                function( tag ) {
-                                    output( "        tag: " + tag.tag + "  " + tojson( tag.min ) + " -> " + tojson( tag.max ) );
+                        if ( totalChunks < 20 || verbose ) {
+                            configDB.chunks.find( { "ns" : coll._id } ).sort( { min : 1 } ).forEach(
+                                function(chunk) {
+                                    output( "        " +
+                                        tojson( chunk.min, 0, true) + " -> " +
+                                        tojson( chunk.max, 0, true ) +
+                                        " on: " + colorize(chunk.shard, {color: "cyan"}) + " " + tojson( chunk.lastmod ) + " " +
+                                        ( chunk.jumbo ? "jumbo " : "" )
+                                    );
                                 }
                             );
                         }
+                        else {
+                            output( "\t\t\ttoo many chunks to print, use verbose if you want to force print" );
+                        }
+
+                        configDB.tags.find( { ns : coll._id } ).sort( { min : 1 } ).forEach(
+                            function( tag ) {
+                                output( "        tag: " + tag.tag + "  " + tojson( tag.min ) + " -> " + tojson( tag.max ) );
+                            }
+                        );
                     }
-                );
-            }
-        }
+                }
+            );
+        } }
     );
 
     print( raw );
