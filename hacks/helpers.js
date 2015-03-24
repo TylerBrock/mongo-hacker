67717220e8b957774135d0854dad4ffca7084214
Gianfranco <gianpa@gmail.com>
2015-03-24 00:44:29 +0000

    jshint helpers.js

--- hacks/helpers.js
+++ hacks/helpers.js
@@ -7,21 +7,21 @@ function runMatch(cmd, args, regexp) {
     }
     var output = rawMongoProgramOutput();
     return output.match(regexp);
-};
+}
 
 function getEnv(env_var) {
-    var env_regex = new RegExp(' ' + env_var + '=(.*)');
-    return runMatch('env', '', env_regex)[1];
-};
+    var env_regex = new RegExp(" " + env_var + "=(.*)");
+    return runMatch("env", "", env_regex)[1];
+}
 
 function getVersion() {
     var regexp = /version: (\d).(\d).(\d)/;
-    return runMatch('mongo', '--version', regexp).slice(1, 4);
-};
+    return runMatch("mongo", "--version", regexp).slice(1, 4);
+}
 
 function isMongos() {
-    return db.isMaster().msg == 'isdbgrid';
-};
+    return db.isMaster().msg === "isdbgrid";
+}
 
 function getSlowms(){
     if(!isMongos()){
@@ -29,4 +29,4 @@ function getSlowms(){
     } else {
         return 100;
     }
-};
+}
