986d8bcaf876518908bc460cd275f4f07f8031a7
Peter Vandenberk <pvandenberk@mac.com>
2016-01-28 11:46:42 +0000

    Feature: delta counts for "count documents" cmd

--- hacks/count.js
+++ hacks/count.js
@@ -1,3 +1,19 @@
+function delta(currentCount, previousCount) {
+    if (!mongo_hacker_config['count_deltas']) {
+        return "";
+    }
+    var delta = Number(currentCount - previousCount);
+    delta = isNaN(delta) ? colorize("(first count)", { color: 'blue' }) :
+        (delta == 0) ?  colorize("(=)", { color: 'blue' }) :
+            (delta > 0) ? colorize("(+" + delta.commify() + ")", { color: 'green' }) :
+                (delta < 0) ? colorize("(" + delta.commify() + ")", { color: 'red' }) :
+                    (delta + " not supported");
+    return " / " + delta.pad(21); // FIXME use "dynamic" padding
+}
+
+// global variable (to ensure "persistence")
+shellHelper.previousDocumentCount = {};
+
 // "count documents", a bit akin to "show collections"
 shellHelper.count = function (what) {
     assert(typeof what == "string");
@@ -22,8 +38,14 @@ shellHelper.count = function (what) {
             return !collectionName.startsWith('system.');
         });
         documentCounts = collectionNames.map(function (collectionName) {
+            // retrieve the previous document count for this collection
+            var previous = shellHelper.previousDocumentCount[collectionName];
+            // determine the current document count for this collection
             var count = db.getCollection(collectionName).count();
-            return (count.commify() + " document(s)");
+            // update the stored document count for this collection
+            shellHelper.previousDocumentCount[collectionName] = count;
+            // format the current document count, incl. delta since last count
+            return (count.commify() + " document(s)" + delta(count, previous));
         });
         printPaddedColumns(collectionNames, documentCounts, mongo_hacker_config['colors']['collectionNames']);
         return "";
